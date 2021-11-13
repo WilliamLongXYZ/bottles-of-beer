b6a444a71f889a1ac74e2d013db35d3ab15fdc6d
William Long <will@williamlong.xyz>
2021-11-13 14:53:57 +0000

    Made basic program Added functionality to print the lyrics and subtract bottles, all organized into a main() function

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ Bottles of Beer on the Wall
 Inspired by The Big Book of Small Python Projects
 Reformatted and changed by William Long <williamlong0726@gmail.com
 '''
+
+def main():
+    bottles = 99
+    while bottles:
+        print(f"{bottles} bottles of beer on the wall")
+        print(f"{bottles} bottles of beer")
+        print(f"Take one down, pass it around")
+        print(f"{bottles-1} bottles of beer on the wall")
+        bottles -= 1
+
+if __name__ == "__main__":
+    main()
