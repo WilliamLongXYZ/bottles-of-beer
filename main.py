78fd5b79d0d609d0d2bb4ce84ff06bda28b42d07
William Long <will@williamlong.xyz>
2021-11-13 19:23:06 +0000

    Updated .gitignore and fixed bottle handling Made variables for words that change depending on the number of bottles, so that I could shrink the if-statements.

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ Bottles of Beer on the Wall
 Inspired by The Big Book of Small Python Projects
 Reformatted and changed by William Long <williamlong0726@gmail.com
 '''
+import time
 
 def main():
     bottles = 99
     while bottles:
+        word = "bottle"
+        it = "it"
         if bottles != 1:
-            print(f"{bottles} bottles of beer on the wall")
-            print(f"{bottles} bottles of beer")
-            print(f"Take one down, pass it around")
-        else:
-            print(f"{bottles} bottle of beer on the wall")
-            print(f"{bottles} bottle of beer")
-            print(f"Take it down, pass it around")
-        print(f"{bottles-1} bottles of beer on the wall") if bottles-1 != 1 else print(f"{bottles-1} bottle of beer on the wall")
-        print()
+            word += "s"
+            it = "one"
+        print(f"{bottles} {word} of beer on the wall")
+        time.sleep(2)
+        print(f"{bottles} {word} of beer")
+        time.sleep(2)
+        print(f"Take {it} down, pass it around")
         bottles -= 1
+        time.sleep(2)
+        print(f"{bottles} {word} of beer on the wall")
+        time.sleep(3)
+        print()
 
 if __name__ == "__main__":
     main()
