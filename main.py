642a57ae84c374f11a4526ba43c19ff7d791f302
William Long <will@williamlong.xyz>
2021-11-13 19:12:49 +0000

    Changed check for last verse Reformatted if statement

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ Reformatted and changed by William Long <williamlong0726@gmail.com
 def main():
     bottles = 99
     while bottles:
-        print(f"{bottles} bottles of beer on the wall")
-        print(f"{bottles} bottles of beer")
-        print(f"Take one down, pass it around")
-        print(f"{bottles-1} bottles of beer on the wall")
+        if bottles != 1:
+            print(f"{bottles} bottles of beer on the wall")
+            print(f"{bottles} bottles of beer")
+            print(f"Take one down, pass it around")
+        else:
+            print(f"{bottles} bottle of beer on the wall")
+            print(f"{bottles} bottle of beer")
+            print(f"Take it down, pass it around")
+        print(f"{bottles-1} bottles of beer on the wall") if bottles-1 != 1 else print(f"{bottles-1} bottle of beer on the wall")
+        print()
         bottles -= 1
 
 if __name__ == "__main__":
     main()
+
